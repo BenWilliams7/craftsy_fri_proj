cc4e81d33116548ec007d8bc2a86789b065112f0
Guest User <Guest@Epicodus-2A.local>
2017-07-11 15:29:31 -0700

    establish admins

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,23 +8,5 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      # flash[:notice] = "Product sucessfully added."
-      redirect_to  products_path
-    else
-      render :new
-    end
-  end
-
-  private
-  def product_params
-    params.require(:product).permit(:name, :price, :description)
-  end
 
 end
